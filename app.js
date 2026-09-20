ffc81416d1ce831825b53ce688e49d3cbed308ec
Kangna Gupta <kangnagupta1225@gmail.com>
2026-09-20 19:50:00 +0530

    Deployed finally

--- app.js
+++ app.js
@@ -5,22 +5,6 @@ const msg=document.querySelector("#msg");
 const userScorePara=document.querySelector("#user-score");
 const compScorePara=document.querySelector("#comp-score");
 
-/*let modebtn=document.getElementById("#themeBtn");
-let currmode="light";
-modebtn.addEventListener("click",()=>{
-    if(currmode==="light"){
-        currmode="dark";
-        document.querySelector("body").style.backgroundColor="black";
-        document.querySelector("body").style.color="white";
-    }
-    else{
-        currmode="light";
-        document.querySelector("body").style.backgroundColor="white";
-        document.querySelector("body").style.color="black";
-    }
-    console.log(currmode);
-});*/
-
 const themeToggle = document.getElementById("themeBtn");
 themeToggle.addEventListener("click", () => {
     document.body.classList.toggle("dark");
