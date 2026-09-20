877619d6ca74ae250ae304e4264b4c3cb29571fa
Kangna Gupta <kangnagupta1225@gmail.com>
2026-09-20 19:40:10 +0530

    Final deployment

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const msg=document.querySelector("#msg");
 const userScorePara=document.querySelector("#user-score");
 const compScorePara=document.querySelector("#comp-score");
 
-let modebtn=document.querySelector("#themeBtn");
+let modebtn=document.getElementById("#themeBtn");
 let currmode="light";
 modebtn.addEventListener("click",()=>{
     if(currmode==="light"){
